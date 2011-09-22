3efa8537775964b96e286ba45184950416f18a41
akarnokd <akarnokd@ab9a2146-0e2c-42a8-a2d4-219d01c8c0be>
2011-09-22 06:10:14 +0000

    Removed unused GWT objects

--- advance/src/eu/advance/logistics/web/server/AdvanceProjectServiceImpl.java
+++ advance/src/eu/advance/logistics/web/server/AdvanceProjectServiceImpl.java
@@ -20,38 +20,17 @@
  */
 package eu.advance.logistics.web.server;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
-import eu.advance.logistics.web.model.AdvanceProjectService;
-import eu.advance.logistics.xml.XML;
 
 /**
  * The server side implementation of the advance project services.
  * @author karnokd
  *
  */
-public class AdvanceProjectServiceImpl extends RemoteServiceServlet implements
-		AdvanceProjectService {
+public class AdvanceProjectServiceImpl extends RemoteServiceServlet {
 	/** The logger. */
 	static final Logger LOG = LoggerFactory.getLogger(AdvanceProjectServiceImpl.class.getName());
 	/**
@@ -59,64 +38,4 @@ public class AdvanceProjectServiceImpl extends RemoteServiceServlet implements
 	 */
 	private static final long serialVersionUID = 6929308684463332904L;
 	
-	@Override
-	public Map<String, String> getLabels(String languageCode) {
-		Map<String, String> result = Maps.newHashMap();
-
-		if (getLanguages().contains(languageCode)) {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			try {
-				DocumentBuilder db = dbf.newDocumentBuilder();
-				InputStream in = getClass().getResourceAsStream("/labels_" + languageCode + ".xml");
-				if (in != null) {
-					try {
-						Element root = db.parse(in).getDocumentElement();
-						for (Element entry : XML.childrenWithName(root, "entry")) {
-							result.put(entry.getAttribute("key"), entry.getTextContent());
-						}
-					} finally {
-						in.close();
-					}
-				} else {
-					LOG.error("Missing label file: " + "/labels_" + languageCode + ".xml");
-				}
-			} catch (ParserConfigurationException ex) {
-				LOG.error(ex.toString(), ex);
-			} catch (IOException ex) {
-				LOG.error(ex.toString(), ex);
-			} catch (SAXException ex) {
-				LOG.error(ex.toString(), ex);
-			}
-		}
-		return result;
-	}
-	@Override
-	public List<String> getLanguages() {
-		URL englishResource = getClass().getResource("/");
-		List<String> result = Lists.newArrayList();
-		
-		if (englishResource != null) {
-			try {
-				File dir = new File(englishResource.toURI());
-				if (dir != null) {
-					File[] files = dir.listFiles(new FilenameFilter() {
-						@Override
-						public boolean accept(File dir, String name) {
-							return name.startsWith("labels_") && name.endsWith(".xml");
-						}
-					});
-					if (files != null) {
-						for (File f : files) {
-							String n = f.getName();
-							result.add(n.substring(7, n.length() - 4));
-						}
-					}
-				}
-			} catch (URISyntaxException e) {
-				LOG.error(e.toString(), e);
-			}
-		}
-		
-		return result;
-	}
 }
