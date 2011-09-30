abec61c7076187251a3331d840fb8fb52182e801
dalmaso <dalmaso@ab9a2146-0e2c-42a8-a2d4-219d01c8c0be>
2011-09-30 15:27:28 +0000

    Initial support for Flow Engine Control Center.

---
+++ flow-editor/trunk/FlowEngineCC/src/eu/advance/logistics/flow/engine/controlcenter/LoginAction.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010-2012 The Advance EU 7th Framework project consortium
+ *
+ * This file is part of Advance.
+ *
+ * Advance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Advance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Advance.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+package eu.advance.logistics.flow.engine.controlcenter;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionID;
+
+@ActionID(category = "RemoteFlowEngine",
+id = "eu.advance.logistics.flow.engine.controlcenter.LoginAction")
+@ActionRegistration(displayName = "#CTL_LoginAction")
+@ActionReferences({
+    @ActionReference(path = "Menu/RemoteFlowEngine", position = 100)
+})
+public final class LoginAction implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
