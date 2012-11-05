1a3c8b35e981a34d5e673a527e74e33ffc22a58e
David Karnok <akarnokd@users.noreply.github.com>
2012-11-05 09:24:36 +0000

    Fixed block-bind loading problem. Added CreateCollection with varargs. Added getAll convenience method to get all varargs values

---
+++ advance/src/eu/advance/logistics/flow/engine/block/projecting/CreateCollection.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010-2013 The Advance EU 7th Framework project consortium
+ *
+ * This file is part of Advance.
+ *
+ * Advance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Advance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Advance.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package eu.advance.logistics.flow.engine.block.projecting;
+
+import java.util.List;
+
+import eu.advance.logistics.annotations.Block;
+import eu.advance.logistics.annotations.Input;
+import eu.advance.logistics.annotations.Output;
+import eu.advance.logistics.flow.engine.block.AdvanceBlock;
+import eu.advance.logistics.flow.engine.xml.XElement;
+
+/**
+ * Wraps the value into an option.
+ * @author karnokd, 2012.02.24.
+ */
+@Block(id = "CreateCollection", category = "projection", 
+scheduler = "NOW", 
+description = "Creates a collection from the supplied variable number of arguments.",
+parameters = { "T" }
+)
+public class CreateCollection extends AdvanceBlock {
+    /** In. */
+    @Input(value = "?T", variable = true)
+    protected static final String IN = "in";
+    /** Out. */
+    @Output("advance:collection<?T>")
+    protected static final String OUT = "out";
+    @Override
+    protected void invoke() {
+    	List<XElement> all = getAll(IN);
+    	dispatch(OUT, resolver().create(all));
+    }
+}
